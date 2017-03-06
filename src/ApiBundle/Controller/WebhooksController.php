864d49a733bf05cccd77bf5e6d39f779f11fc8a4
Ivan Alex <alex.syscore@gmail.com>
2017-03-06 14:06:58 +0200

    much stuff

--- src/ApiBundle/Controller/WebhooksController.php
+++ src/ApiBundle/Controller/WebhooksController.php
@@ -4,17 +4,30 @@ namespace ApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Stripe\Stripe;
 
 class WebhooksController extends Controller
 {
     /**
-     * @Route("/paymentBounce")
+     * @Route("/paymentbounce")
      */
     public function paymentBounceAction()
     {
-        return $this->render('ApiBundle:Webhooks:payment_bounce.html.twig', array(
+		Stripe::setApiKey($this->container->getParameter('secret_key'));
+
+		$input = @file_get_contents("php://input");
+		$event_json = json_decode($input);
+
+		$response = new Response();
+		$response->setStatusCode(Response::HTTP_OK);
+
+		return $response;
+
+/*        return $this->render('ApiBundle:Webhooks:payment_bounce.html.twig', array(
             // ...
         ));
-    }
+*/    }
 
 }
