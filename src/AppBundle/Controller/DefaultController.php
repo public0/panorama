1653f7dcdb2f580c8626067e64c8692c17ad6340
Ivan Alex <alex.syscore@gmail.com>
2017-04-19 09:39:41 +0300

    terms

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -10,6 +10,7 @@ use Stripe\Stripe;
 use Payum\Stripe\Request\Api\CreateCustomer;
 use AppBundle\Entity\Trial;
 use AppBundle\Form\TrialType;
+use AppBundle\Form\ContactType;
 
 class DefaultController extends Controller
 {
@@ -33,6 +34,16 @@ class DefaultController extends Controller
     }
 
     /**
+     * @Route("/terms", name="terms")
+     */
+    public function termsAction(Request $request)
+    {
+        return $this->render('default/terms.html.twig', [
+        ]);
+    }
+
+
+    /**
      * @Route("/about", name="about")
      */
     public function aboutAction(Request $request)
@@ -46,7 +57,71 @@ class DefaultController extends Controller
      */
     public function contactAction(Request $request)
     {
+
+        $form = $this->createForm(ContactType::class, array(
+            // To set the action use $this->generateUrl('route_identifier')
+            'action' => $this->generateUrl('contact'),
+            'method' => 'POST'
+        ));
+
+        if ($request->isMethod('POST')) {
+            // Refill the fields in case the form is not valid.
+            $form->handleRequest($request);
+
+            if($form->isValid()){
+                // Send mail
+
+                if($this->sendEmail($form->getData())){
+
+                    // Everything OK, redirect to wherever you want ! :
+                   return $this->redirectToRoute('contact', ['success' => true]);
+                }else{
+                    // An error ocurred, handle
+                   return $this->redirectToRoute('contact', ['success' => false]);
+                }
+            }
+        }
+
+        return $this->render('default/contact.html.twig', array(
+            'form' => $form->createView()
+        ));
+
         return $this->render('default/contact.html.twig', [
         ]);
     }
+
+    /**
+     * @Route("/privacy-policy", name="privacy")
+     */
+    public function privacyAction(Request $request)
+    {
+        return $this->render('default/privacy.html.twig', [
+        ]);
+    }
+
+    private function sendEmail($data){
+
+        $email = $this->container->getParameter('mailer_user');
+        $password = $this->container->getParameter('mailer_password');
+
+        $transport = new \Swift_SmtpTransport("smtp.office365.com", 587);
+        $transport->setUsername($email);
+        $transport->setPassword($password);
+        $transport->setEncryption('tls');
+        
+        $mailer = new \Swift_Mailer($transport);
+
+        $message = new \Swift_Message();
+
+        $message->setSubject($data['subject'])
+            ->setFrom($this->container->getParameter('mailer_user'))
+            ->setTo($data['name'])
+            ->setBody(
+                $data['message']
+            );
+
+        $mailer->send($message);
+
+    }
+
 }
